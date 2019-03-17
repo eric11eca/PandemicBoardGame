94e2e38544e4f37460742564c1cacf32de173104
eric11eca <eric11eca@gmail.com>
2019-03-17 14:37:36 -0400

    testCoordinates passes

---
+++ src-main/City.java
@@ -0,0 +1,23 @@
+
+public class City {
+	private int X;
+	private int Y;
+
+	public City() {}
+	
+	public int getX() {
+		return this.X;
+	}
+	
+	public int getY() {
+		return this.Y;
+	}
+	
+	public void setX(int X) {
+		this.X = X;
+	}
+	
+	public void setY(int Y) {
+		this.Y = Y;
+	}
+}
---
+++ src-test/TestCity.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCity {
+
+	@Test
+	public void testCoordinates() {
+		City city = new City();
+		city.setX(12);
+		city.setY(50);
+		assertEquals(city.getX(), 12);
+		assertEquals(city.getY(), 50);
+	}
+
+}
