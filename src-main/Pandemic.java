4adeeffd0c871e06ef5a5a2bc16a8806811ca965
mahajaa1 <mahajaa1@mahajaa1-5520.rose-hulman.edu>
2019-03-17 14:33:54 -0400

    Player class started, has errors since there is no Card class.

--- src-main/Pandemic.java
+++ src-main/Pandemic.java
@@ -1,4 +1,13 @@
 
 public class Pandemic {
+	
+	public static void main() {
+        startGame();
+    }
+
+	private static void startGame() {
+		//Build all the objects
+		//Call the GUI to select the number of players
+	}
 
 }
