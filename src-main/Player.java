d46852d8c6e4f3635e60e351eba5da857ccb9696
Yxu <xuy8@rose-hulman.edu>
2019-03-19 19:44:39 -0400

    testMove passes

--- src-main/Player.java
+++ src-main/Player.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public abstract class Player {
 	
 	public ArrayList<PlayerCard> hand;
-
+	public City location;
 
 	public void receiveCard(PlayerCard playercard) {
+		if(hand.size() >= 7){
+			// somehow discard card
+		}
 		hand.add(playercard);
 	}
-
 
 	public boolean discardCard(String cardname){
 		for(int i = 0; i < hand.size();i++){
@@ -22,13 +24,19 @@ public abstract class Player {
 	
 	public boolean handContains(String cardname){
 		for(PlayerCard playercard : hand){
-			if(playercard.getName().equals("Beijing")){
+			if(playercard.getName().equals(cardname)){
 				return true;
 			}
 		}
 		return false;
 	}
-	
 
+	public boolean move(City city) {
+		// TODO Auto-generated method stub
+		location = city;
+		return true;
+	}
+	
+	
 	
 }
--- src-test/PlayerTest.java
+++ src-test/PlayerTest.java
@@ -1,7 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 public class PlayerTest {
@@ -10,10 +8,27 @@ public class PlayerTest {
 	@Test 
 	public void testNormalReceiveAndDiscardCityCard(){
 		Player player = new Dispatcher();
-		player.receiveCard(new CityCard("Beijing"));
-		assertTrue(player.handContains("Beijing"));
-		player.discardCard("Beijing");
-		assertFalse(player.handContains("Beijing"));
+		player.receiveCard(new CityCard("A"));
+		assertTrue(player.handContains("A"));
+		player.discardCard("A");
+		assertFalse(player.handContains("A"));
 	}
 	
+	@Test 
+	public void testReceiveCardWithExceedNumberOfHandCard(){
+		Player player = new Dispatcher();
+		String[] cities = {"A","B","C","D","E","F","G"};
+		for(String city: cities){
+			player.receiveCard(new CityCard(city));
+		}
+		player.receiveCard(new CityCard("Q"));
+		assertEquals(7,player.hand.size());
+	}
+	@Test 
+	public void testMove(){
+		Player player = new Dispatcher();
+		City city = new City();
+		player.move(city);
+		assertEquals(player.location, city);
+	}
 }
