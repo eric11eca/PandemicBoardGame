252f7176642be1c721404b0d325ec4add9e35e93
eric11eca <eric11eca@gmail.com>
2019-03-21 04:22:01 -0400

    testInitializeDiseaseCube3 passes

--- src-test/TestInitializeBoard.java
+++ src-test/TestInitializeBoard.java
@@ -119,7 +119,38 @@ public class TestInitializeBoard {
 		assertTrue(3 == city.diseaseCubes.get(color));
 	}
 	
-	
+	@Test
+	public void testInitializeDiseaseCube3() {
+		initializeBoard.board.validInfectionCard.add("a");
+		initializeBoard.board.validInfectionCard.add("b");
+		initializeBoard.board.validInfectionCard.add("c");
+		initializeBoard.board.validInfectionCard.add("d");
+		initializeBoard.board.validInfectionCard.add("e");
+		initializeBoard.board.validInfectionCard.add("f");
+		initializeBoard.board.validInfectionCard.add("g");
+		initializeBoard.board.validInfectionCard.add("h");
+		initializeBoard.board.validInfectionCard.add("i");
+		initializeBoard.board.validInfectionCard.add("j");
+		
+		initializeBoard.board.cities.put("a", new City("a"));
+		initializeBoard.board.cities.put("b", new City("b"));
+		initializeBoard.board.cities.put("c", new City("c"));
+		initializeBoard.board.cities.put("d", new City("d"));
+		initializeBoard.board.cities.put("e", new City("e"));
+		initializeBoard.board.cities.put("f", new City("f"));
+		initializeBoard.board.cities.put("g", new City("g"));
+		initializeBoard.board.cities.put("h", new City("h"));
+		initializeBoard.board.cities.put("i", new City("i"));
+		initializeBoard.board.cities.put("j", new City("j"));
+		
+		initializeBoard.initializeDiseaseCube();
+		List<String> discardInfectionCard = initializeBoard.board.discardInfectionCard;
+		Map<String, City> cities = initializeBoard.board.cities;
+		String cardName = discardInfectionCard.get(5);
+		City city = cities.get(cardName);
+		String color = city.color;
+		assertTrue(2 == city.diseaseCubes.get(color));
+	}
 	
 	@Test
 	public void testInitializePlayerCard() {
