272f92b836ff63ff1446f6b63603ac60940b26b1
Yxu <xuy8@rose-hulman.edu>
2019-03-31 15:20:44 -0400

    testDrive passes. Checked drive both neighbor and not neighbor.

--- src-main/Player/Player.java
+++ src-main/Player/Player.java
@@ -1,38 +1,46 @@
 package Player;
+
 import java.util.ArrayList;
 
 import Card.PlayerCard;
 import Initialize.City;
 
-public abstract class Player{
+public abstract class Player {
 
 	public ArrayList<PlayerCard> hand = new ArrayList<>();
 	public City location;
 
 	public void receiveCard(PlayerCard playercard) {
-		if(hand.size() >= 7){
+		if (hand.size() >= 7) {
 			// somehow discard card
 		}
-		hand.add(playercard);     
+		hand.add(playercard);
 	}
-	
-	public boolean discardCard(PlayerCard playercard){
-		if(hand.contains(playercard)){
+
+	public boolean discardCard(PlayerCard playercard) {
+		if (hand.contains(playercard)) {
 			hand.remove(playercard);
 			return true;
 		}
 		return false;
 	}
 
+	public void isValid() {
+
+	}
+
+	public void drive(City destination) {
+		if (location.neighbors.contains(destination)) {
+			location = destination;
+		} else {
+			throw new RuntimeException("Invalid destinaion: Not a neighbor!!");
+		}
+	}
+
 	public void move(City city) {
-		if(location.neighbors.contains(city)){
+		if (location.neighbors.contains(city)) {
 			location = city;
 		}
 	}
-	
-	
-	
-	
-	
-	
+
 }
