0379fd1174e57ab94d103485475ff484ba602f39
eric11eca <eric11eca@gmail.com>
2019-04-04 04:23:03 -0400

    testDiscoverCureScientist passes

--- src-main/Player/DiscoverCureScientist.java
+++ src-main/Player/DiscoverCureScientist.java
@@ -4,28 +4,48 @@ import java.util.Map;
 import java.util.Set;
 
 import Card.PlayerCard;
+import Initialize.Board;
 
 public class DiscoverCureScientist implements DiscoverCure {
 	private Map<String, PlayerCard> hand;
-	private String disease;
 	private Set<String> curedDiseases;
+	private int red_count = 0;
+	private int blue_count = 0;
+	private int black_count = 0;
+	private int yellow_count = 0;
 	
-	public DiscoverCureScientist(Map<String, PlayerCard> hand, String disease, Set<String> curedDiseases) {
+	public DiscoverCureScientist(Map<String, PlayerCard> hand, Set<String> curedDiseases) {
 		this.hand = hand;
-		this.disease = disease;
 		this.curedDiseases = curedDiseases;
 	}
 
 	@Override
 	public void discoverCure() {
-		int count = 0;
-		for(String cardName : hand.keySet()) {
-			if(hand.get(cardName).color == disease) {
-				count++;
-			}
+		countColors();
+		if(red_count >= 4) {
+			curedDiseases.add("Red");
+		} else if (blue_count >= 4) {
+			curedDiseases.add("Blue");
+		} else if (black_count >= 4) {
+			curedDiseases.add("Black");
+		} else if (yellow_count >= 4) {
+			curedDiseases.add("Yellow");
 		}
-		if(count >= 4) {
-			curedDiseases.add(disease);
+	}
+	
+	private void countColors() {
+		for(PlayerCard card : hand.values()) {
+			if(card.cardType == Board.CardType.CITYCARD) {
+				if(card.color == "Yellow") {
+					yellow_count++;
+				} else if (card.color == "Red") {
+					red_count++;
+				} else if (card.color == "Blue") {
+					blue_count++;
+				} else if (card.color == "Black") {
+					black_count++;
+				}
+			}
 		}
 	}
 }
---
+++ src-test/TestPlayers/TestDiscoverCureScientist.java
@@ -0,0 +1,64 @@
+package TestPlayers;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Card.PlayerCard;
+import Initialize.Board;
+import Player.DiscoverCureScientist;
+
+public class TestDiscoverCureScientist {
+	DiscoverCureScientist scienceCure;
+	Map<String, PlayerCard> hand;
+	Board board;
+	
+	@Before
+	public void setup() {
+		hand = new HashMap<>();
+		board = new Board();
+		scienceCure = new DiscoverCureScientist(hand, board.curedDiseases);
+	}
+
+	@Test
+	public void testDiscoverCureNormal() {
+		String cardName1 = "a";
+		String cardName2 = "b";
+		String cardName3 = "c";
+		String cardName4 = "d";
+		String cardName5 = "e";
+		String cardName6 = "f";
+		String cardName7 = "g";
+
+		PlayerCard card1 = new PlayerCard(Board.CardType.CITYCARD, cardName1);
+		PlayerCard card2 = new PlayerCard(Board.CardType.CITYCARD, cardName2);
+		PlayerCard card3 = new PlayerCard(Board.CardType.CITYCARD, cardName3);
+		PlayerCard card4 = new PlayerCard(Board.CardType.CITYCARD, cardName4);
+		PlayerCard card5 = new PlayerCard(Board.CardType.CITYCARD, cardName5);
+		PlayerCard card6 = new PlayerCard(Board.CardType.EVENTCARD, cardName6);
+		PlayerCard card7 = new PlayerCard(Board.CardType.CITYCARD, cardName7);
+		
+		card1.color = "Yellow";
+		card2.color = "Red";
+		card3.color = "Red";
+		card4.color = "Red";
+		card5.color = "Red";
+		card7.color = "Blue";
+		
+		hand.put(cardName1, card1);
+		hand.put(cardName2, card2);
+		hand.put(cardName3, card3);
+		hand.put(cardName4, card4);
+		hand.put(cardName5, card5);
+		hand.put(cardName6, card6);
+		hand.put(cardName7, card7);
+		
+		assertTrue(board.curedDiseases.isEmpty());
+		scienceCure.discoverCure();
+		assertTrue(board.curedDiseases.contains("Red"));
+	}
+}
