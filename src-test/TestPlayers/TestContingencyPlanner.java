80c86f0feba5cf684ab5a5c8a31a593f7df322c7
Yxu <xuy8@rose-hulman.edu>
2019-04-04 21:53:14 -0400

    Merge Eric's branch to master

--- src-test/TestPlayers/TestContingencyPlanner.java
+++ src-test/TestPlayers/TestContingencyPlanner.java
@@ -1,8 +1,8 @@
 package TestPlayers;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,18 +10,16 @@ import org.junit.Test;
 import Card.PlayerCard;
 import Initialize.Board;
 import Player.ContingencyPlanner;
-import Player.Player;
 
 public class TestContingencyPlanner {
 	Board board;
-	ContingencyPlanner contingencyPlanner; 
-	
-	@Before 
+	ContingencyPlanner contingencyPlanner;
+
+	@Before
 	public void setup() {
 		board = new Board();
 		contingencyPlanner = new ContingencyPlanner(board);
 	}
-	
 
 	@Test
 	public void testPickCardFromDispach() {
@@ -34,13 +32,13 @@ public class TestContingencyPlanner {
 		assertEquals(contingencyPlanner.specialEventCard.cardName, cardName);
 		assertFalse(board.discardPlayerCard.containsKey(cardName));
 	}
-	
+
 	@Test
 	public void testRemoveSpecialEventCardCompletly() {
 		String cardName = "Airlift";
 		PlayerCard playerCard = new PlayerCard(Board.CardType.EVENTCARD, cardName);
 		contingencyPlanner.specialEventCard = playerCard;
-		
+
 		String cardName1 = "Chicago";
 		String cardName2 = "New York";
 		PlayerCard playerCard1 = new PlayerCard(Board.CardType.CITYCARD, cardName1);
@@ -48,8 +46,8 @@ public class TestContingencyPlanner {
 		board.discardPlayerCard.put(cardName1, playerCard1);
 		board.discardPlayerCard.put(cardName2, playerCard2);
 		int old_size = board.discardPlayerCard.size();
-		
-		boolean cardUsed = contingencyPlanner.useCard(cardName);
+
+		boolean cardUsed = contingencyPlanner.usePlayerCard(cardName);
 		assertTrue(cardUsed);
 		assertTrue(contingencyPlanner.specialEventCard == null);
 		int new_size = board.discardPlayerCard.size();
