d2abc25aaa50e5b2b7191dbaf41c47f2df9b8274
eric11eca <eric11eca@gmail.com>
2019-04-26 00:53:16 -0400

    killed some mutants in Epidemic Card and Game Action

--- src-main/Card/EpidemicCardAction.java
+++ src-main/Card/EpidemicCardAction.java
@@ -23,39 +23,25 @@ public class EpidemicCardAction {
 	}
 
 	public void infect() {
-		System.out.println("1");
 		int size = board.validInfectionCard.size();
-		System.out.println("2");
 		String infection = board.validInfectionCard.get(size - 1);
-		System.out.println("3");
 		City city = board.cities.get(infection);
-		System.out.println("4");
 		String disease = city.color;
-		System.out.println("5");
 		if (!board.eradicatedDiseases.contains(disease)) {
-			System.out.println("6");
-			int count = 0;
+			int addCubeCount = 0;
 			while (city.diseaseCubes.get(disease) != 3) {
-				System.out.println("7");
 				int remainingCubes = board.remainDiseaseCube.get(disease);
-				System.out.println("8");
 				if (remainingCubes == 0) {
 					board.gameEnd = true;
 					board.playerLose = true;
 					return;
 				}
-				System.out.println("9");
 				int numOfCubes = city.diseaseCubes.get(disease);
-				System.out.println("10");
 				city.diseaseCubes.put(disease, numOfCubes + 1);
-				System.out.println("11");
 				board.remainDiseaseCube.put(disease, remainingCubes - 1);
-				System.out.println("12");
-				count++;
+				addCubeCount++;
 			}
-			System.out.println("13");
-			if (count != 3) {
-				System.out.println("14");
+			if (addCubeCount != 3) {
 				outbreak.performeOutbreak(city);
 			}
 		}
@@ -75,17 +61,12 @@ public class EpidemicCardAction {
 	}
 
 	public void performeEpidemic() {
-		System.out.println("Before Infection Rate");
 		increaseInfectionRate();
-		System.out.println("Before Infection");
 		infect();
-		System.out.println("Before board end");
 		if (board.gameEnd == true) {
 			return;
 		}
-		System.out.println("Before Reshuffle");
 		if (reshuffleDiscardInfectionDeck()) {
-			System.out.println("Before making");
 			makingNewInfectionCardDeck();
 		}
 	}
