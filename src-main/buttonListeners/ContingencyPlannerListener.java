aa5fff9ebc407574e9a3ae65cc9ef53c0d493be7
mahajaa1 <mahajaa1@mahajaa1-5520.rose-hulman.edu>
2019-05-09 18:17:50 -0400

    Contingency Planner FE done, to be connected to the backend

--- src-main/buttonListeners/ContingencyPlannerListener.java
+++ src-main/buttonListeners/ContingencyPlannerListener.java
@@ -1,13 +1,43 @@
 package buttonListeners;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+import initialize.Board;
+import panel.GUI;
+
 public class ContingencyPlannerListener implements ActionListener {
+	
+	private Board board;
+	JPanel panel;
+	GUI gui;
+
+	public ContingencyPlannerListener(Board board, GUI gui) {
+		this.board=board;
+		this.gui=gui;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		String[] cards = board.discardEventCards.toArray(new String[board.discardEventCards.size()]);
+		JComboBox<String> cardList = new JComboBox<String>(cards);
+		
+		cardList.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO implement the action
+			}
+			
+		});
+		
+		panel = new JPanel();
+		panel.add(cardList);
+		gui.addPanel(panel, BorderLayout.CENTER);
 
 	}
 
--- src-main/buttonListeners/DispatcherListener.java
+++ src-main/buttonListeners/DispatcherListener.java
@@ -3,11 +3,28 @@ package buttonListeners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import initialize.Board;
+import panel.GUI;
+
 public class DispatcherListener implements ActionListener {
+
+	private Board board;
+	private GUI gui;
+	private JPanel panel;
+
+	public DispatcherListener(Board board, GUI gui) {
+		this.board=board;
+		this.gui=gui;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		JButton moveToCity = new JButton("Move to another player");
+		JButton moveAsSelf = new JButton("Move as though you're own");
 
 	}
 
