28fc3638e991eae77ed81f683127194eba7bff27
eric11eca <eric11eca@gmail.com>
2019-06-10 17:31:09 +0800

    tried with observer pattern on charterFlight button listener

--- src-main/player/DiscoverCureNormal.java
+++ src-main/player/DiscoverCureNormal.java
@@ -3,7 +3,6 @@ package player;
 import java.util.Set;
 
 public class DiscoverCureNormal extends DiscoverCure {
-
 	public DiscoverCureNormal(Set<String> cured) {
 		curedDiseases = cured;
 		cardCount = 5;
