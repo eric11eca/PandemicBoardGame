d0277756d7231e3c685d90562cd5fe0d087103b4
Michael Zhao <zhaoy12@rose-hulman.edu>
2020-04-04 16:49:56 -0400

    start working on UI

--- src-presentation/main/Pandemic.java
+++ src-presentation/main/Pandemic.java
@@ -1,16 +1,6 @@
 package main;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import game.GameSetup;
-import game.city.City;
-import gui.GameGUI;
-import parse.CityLoader;
-import render.RenderCity;
 
 public class Pandemic {
 
