a5f8e29e567d4575715a6e61760133f6c60a070d
Michael Zhao <zhaoy12@rose-hulman.edu>
2020-04-09 14:59:18 -0400

    add mock interaction

---
+++ src-test/test/MockInteraction.java
@@ -0,0 +1,105 @@
+package test;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import game.GameColor;
+import game.cards.Card;
+import game.city.City;
+import game.player.Player;
+import game.player.PlayerInteraction;
+
+public class MockInteraction implements PlayerInteraction {
+	public MockInteraction() {
+		selectColorFromImpl = this::errBiConsumer;
+		selectPlayerFromImpl = this::errBiConsumer;
+		selectCardsFromImpl = this::errTriConsumer;
+		selectCityFromImpl = this::errBiConsumer;
+		selectCardsToDiscardImpl = this::errTriConsumer;
+		arrangeCardsImpl = this::errBiConsumer;
+	}
+
+	public MockInteraction implementSelectColorFrom(MethodSelectColorFrom selectColorFromImpl) {
+		this.selectColorFromImpl = selectColorFromImpl;
+		return this;
+	}
+
+	public MockInteraction implementSelectPlayerFrom(MethodSelectPlayerFrom selectPlayerFromImpl) {
+		this.selectPlayerFromImpl = selectPlayerFromImpl;
+		return this;
+	}
+
+	public MockInteraction implementSelectCardsFrom(MethodSelectCardsFrom selectCardsFromImpl) {
+		this.selectCardsFromImpl = selectCardsFromImpl;
+		return this;
+	}
+
+	public MockInteraction implementSelectCityFrom(MethodSelectCityFrom selectCityFromImpl) {
+		this.selectCityFromImpl = selectCityFromImpl;
+		return this;
+	}
+
+	public <T extends Card> MockInteraction implementSelectCardsToDiscard(
+			MethodSelectCardsFrom selectCardsToDiscardImpl) {
+		this.selectCardsToDiscardImpl = selectCardsToDiscardImpl;
+		return this;
+	}
+
+	public <T extends Card> MockInteraction implementArrangeCards(MethodArrangeCards arrangeCardsImpl) {
+		this.arrangeCardsImpl = arrangeCardsImpl;
+		return this;
+	}
+
+	//@formatter:off
+	private MethodSelectColorFrom selectColorFromImpl;
+	private MethodSelectPlayerFrom selectPlayerFromImpl;
+	private MethodSelectCardsFrom selectCardsFromImpl;
+	private MethodSelectCityFrom selectCityFromImpl;
+	private MethodSelectCardsFrom selectCardsToDiscardImpl;
+	private MethodArrangeCards arrangeCardsImpl;
+
+	public interface MethodSelectColorFrom extends BiConsumer<Set<GameColor>, Consumer<GameColor>> {	}
+	public interface MethodSelectPlayerFrom extends BiConsumer<List<Player>, Consumer<Player>> {	}
+	public interface MethodSelectCardsFrom {
+		<T extends Card> void selectCardsFrom(int number, List<T> cards, Consumer<List<T>> callback);
+	}
+	public interface MethodSelectCityFrom extends BiConsumer<Set<City>,Consumer<City>>{}
+	public interface MethodArrangeCards{
+		<T extends Card> void arrangeCards(List<T> cards, Consumer<List<T>> callback);
+	}
+	@Override
+	public void selectColorFrom(Set<GameColor> colors, Consumer<GameColor> callback) 
+	{selectColorFromImpl.accept(colors, callback);}
+	@Override
+	public void selectPlayerFrom(List<Player> players, Consumer<Player> callback) 
+	{selectPlayerFromImpl.accept(players, callback);}
+	@Override
+	public <T extends Card> void selectCardsFrom(int number, List<T> cards, Consumer<List<T>> callback)
+	{selectCardsFromImpl.selectCardsFrom(number, cards, callback);}
+	@Override
+	public void selectCityFrom(Set<City> cities, Consumer<City> callback) 
+	{selectCityFromImpl.accept(cities, callback);}
+	@Override
+	public <T extends Card> void selectCardsToDiscard(int number, List<T> cards, Consumer<List<T>> callback) 
+	{selectCardsToDiscardImpl.selectCardsFrom(number, cards, callback);}
+	@Override
+	public <T extends Card> void arrangeCards(List<T> cards, Consumer<List<T>> callback) 
+	{arrangeCardsImpl.arrangeCards(cards, callback);}
+	//@formatter:on
+	private <T, R> void errBiConsumer(T t, R r) {
+		mockFail();
+	}
+
+	private <T, R, I> void errTriConsumer(T t, R r, I i) {
+		mockFail();
+	}
+
+	private void mockFail() {
+		fail("This method is not mocked in PlayerInteraction");
+	}
+
+}
--- src-test/test/playerAction/TestDrive.java
+++ src-test/test/playerAction/TestDrive.java
@@ -1,68 +1,67 @@
 package test.playerAction;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
 import game.cards.Deck;
 import game.city.City;
+import game.city.CitySet;
 import game.player.Player;
 import game.player.PlayerImpl;
-import game.player.PlayerInteraction;
 import game.player.action.Action;
 import game.player.action.ActionDrive;
 import test.MockCityBuilder;
+import test.MockInteraction;
 
 public class TestDrive {
 	Player player;
 	City location, neighborCity, notNeighborCity;
 
 	MockCityBuilder cityFactory = new MockCityBuilder();
+	boolean cbExecuted;
+	MockInteraction interaction;
 
 	@Before
 	public void setup() {
-		cityFactory = cityFactory.name("Atlanta"); 
-		neighborCity = cityFactory.build();
-		cityFactory = cityFactory.name("Chicago"); 
-		cityFactory.neighborSet().add(neighborCity);
-		location = cityFactory.build();
-		cityFactory = cityFactory.name("Shanghai"); 
-		notNeighborCity = cityFactory.build();
-		player = new PlayerImpl(0, location, new Deck<>(), null);
+		// correct
+		MockCityBuilder neighborBuilder = new MockCityBuilder().name("Atlanta");
+		neighborCity = neighborBuilder.build();
+
+		MockCityBuilder locationBuilder = new MockCityBuilder().name("Chicago");
+		location = locationBuilder.build();
+
+		// you can add neighbots after city is built
+		neighborBuilder.neighborSet().add(location);
+		locationBuilder.neighborSet().add(neighborCity);
+
+		notNeighborCity = new MockCityBuilder().name("Shanghai").build();
+
+		cbExecuted = false;
+		interaction = new MockInteraction();
+		interaction.implementSelectCityFrom((citiesToSelectFrom, callback) -> {
+			assertTrue(citiesToSelectFrom.contains(neighborCity));
+			assertFalse(citiesToSelectFrom.contains(location));
+			assertFalse(citiesToSelectFrom.contains(notNeighborCity));
+			callback.accept(neighborCity);
+		});
+		player = new PlayerImpl(0, location, new Deck<>(), interaction);
 	}
 
 	@Test
 	public void testNeighborDrive() {
-		Action drive = EasyMock.mock(ActionDrive.class);
-		PlayerInteraction interaction = EasyMock.mock(PlayerInteraction.class);
 		Set<City> citySet = new HashSet<>();
-		citySet.add(location);
-		
-		drive.perform(null);
-		EasyMock.expectLastCall().andAnswer(()->{
-			interaction.selectCityFrom(citySet, null);
-			return null;
-		});
-		
-		interaction.selectCityFrom(citySet, null);
-		EasyMock.expectLastCall().andAnswer(()->{
-			performDriveAction(neighborCity, null);
-			return null;
-		});
-		
-		EasyMock.replay(drive, interaction);
-		drive.perform(null);
-		assertEquals(player.getLocation().getName(), "Atlanta");
-		EasyMock.verify(drive, interaction);
-	}
-	
-	private void performDriveAction(City destination, Runnable completionCallback) {
-		player.setLocation(destination);
-		completionCallback.run();
+		citySet.add(neighborCity);
+
+		Action action = new ActionDrive(new CitySet(citySet), player, interaction);
+		action.perform(() -> cbExecuted = true);
+		assertTrue(cbExecuted);
+		assertEquals(player.getLocation(), neighborCity);
 	}
 }
